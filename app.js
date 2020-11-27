77623fc9b3fea980d30e6cdcb75b67421eb41030
Soulless2k17 <Adam.Lopez2k12@gmail.com>
2020-11-27 21:09:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
 document.querySelector(".runBtn").addEventListener("click", init);
 
 function init() {
-    let lines = [];
-    let roomSize = [];
-    let robots = [];
-    let dangerZone = [{
-        coordX: "",
-        coordY: ""
-    }];
-    let inDanger;
+   let lines = [];
+let roomSize = [];
+let robots = [];
+let dangerZone = [{
+    coordX: "",
+    coordY: ""
+}];
+let inDanger;
 
+function init() {
     function textCleanup() {
         var text = document.querySelector('.input').value;
         text = text.replace(/(?:(?:\r\n|\r|\n)\s*){2}/gm, "");
         document.querySelector('.input').value = text;
-        for (var i = 0; i < document.querySelector('.input').value.split(/\n/).length; i++) {
+        for (var i = 0; i < document.querySelector('.input').value.split(/\n/).length; i++) {          
             let line = document.querySelector('.input').value.split(/\n/)[i];
-            if (line) {
-                lines.push(line.replace(/\s/g, ''));
+            if (line) {                
+                lines.push(line);
             } else {
                 lines.push("");
             }
         }
+        console.log(lines);
         createScene();
     }
 
     function createScene() {
         for (var i = 0; i < lines.length; i++) {
             if (i === 0) {
-                roomSize.push(lines[i][0]);
-                roomSize.push(lines[i][1]);
+                roomSize.push(lines[i].split(' ')[0]);
+                roomSize.push(lines[i].split(' ')[1]);
             } else {
                 if (i % 2 == 0) {
                     robots[robots.length - 1]["orders"] = lines[i];
                 } else {
                     robots.push({
-                        "coordX": parseInt(lines[i][0]),
-                        "coordY": parseInt(lines[i][1]),
-                        "orientation": lines[i][2]
+                        "coordX": parseInt(lines[i].split(' ')[0]),
+                        "coordY": parseInt(lines[i].split(' ')[1]),
+                        "orientation": lines[i].split(' ')[2]
                     });
                 }
-            }
+            }          
         }
         createJourney();
 
