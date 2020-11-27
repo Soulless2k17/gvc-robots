04d8d09f946036fa90a116ad58dcc39792e95a3c
Soulless2k17 <Adam.Lopez2k12@gmail.com>
2020-11-27 18:38:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 let roomSize = [];
 let robots = [];
 
+document.querySelector(".runBtn").addEventListener("click", init);
+
 function init() {
+   function textCleanup() {
+        var text = document.querySelector('.input').value;
+        text = text.replace(/(?:(?:\r\n|\r|\n)\s*){2}/gm, "");
+        document.querySelector('.input').value = text;
+        for (var i = 0; i < document.querySelector('.input').value.split(/\n/).length; i++) {
+            let line = document.querySelector('.input').value.split(/\n/)[i];
+            if (line) {
+                lines.push(line.replace(/\s/g, ''));
+            } else {
+                lines.push("");
+            }
+        }
+        console.log(lines);     
+    }
   
 }  
